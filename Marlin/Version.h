2fe70c2205588dedd291ba56d3a1498a7ca67948
Ramiro del Corro <ramiro.del.corro@gmail.com>
2019-09-23 20:39:15 -0700

    Bump the PWM setting to avoid light flicker

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,20 +41,20 @@
    * Defines the version of the Marlin-AI3M build. Not to be confused with
    * Marlin's own build number, e.g. 1.1.9.
    */
-  #define CUSTOM_BUILD_VERSION "v1.4.6"
+  #define CUSTOM_BUILD_VERSION "v1.4.7"
 
   /**
    * Verbose version identifier which should contain a reference to the location
    * from where the binary was downloaded or the source code was compiled.
    */
-  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (Github, davidramiro)"
+  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (Github, rdelcorro)"
 
   /**
    * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2019-04-03"
+  #define STRING_DISTRIBUTION_DATE "2019-09-23"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
@@ -84,7 +84,7 @@
    * has a distinct Github fork— the Source Code URL should just be the main
    * Marlin repository.
    */
-  #define SOURCE_CODE_URL "https://github.com/davidramiro/Marlin-AI3M"
+  #define SOURCE_CODE_URL "https://github.com/rdelcorro/Marlin-Ai3M"
 
   /**
    * Default generic printer UUID.
